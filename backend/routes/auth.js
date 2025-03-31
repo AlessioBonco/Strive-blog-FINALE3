0c6f91f62a5b160a446925372258742e0a2ad660
AlessioBonco <alessiofrancescob@gmail.com>
2025-03-31 13:26:10 +0200

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/Users');
 
 const generateToken = (user) => {
